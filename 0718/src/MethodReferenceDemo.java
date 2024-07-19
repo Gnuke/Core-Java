654beb72ac93158ee345ffc87416e28bb2caf177
Gnuke <dnr1297@nate.com>
2024-07-19 10:19:43 +0900

    0718 JAVA

--- 0718/src/MethodReferenceDemo.java
+++ 0718/src/MethodReferenceDemo.java
@@ -15,7 +15,7 @@ public class MethodReferenceDemo {
 		MyPolygon mp1 = Math::toDegrees; // 메소드 참조
 		// lambda expression에서 사용하는 메소드의 구현부가 특정 클래스에서 단 하나의 메소드만 사용한다는 것을 보장한다면 
 		// 메소드 참조 사용 가능 -> 클래스명 :: 메소드명;
-		double result1 = mp.radianToDegree(1);
+		double result1 = mp1.radianToDegree(1);
 		System.out.println(result1);
 	}
 }
